5fa4840ee077ab4ce444f49c36ac27119c0e7fb9
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-08-19 18:17:08 +0200

    appsidebar fix

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,24 @@
-<script setup>
+<script>
+import AppSidebar from './components/AppSidebar.vue';
+import AppChatSection from '';
+
+export default {
+  components: { AppSidebar },
+}
 </script>
 
 <template>
-  <div class="container">
+  <div class="h-100 d-flex align-items-center justify-content-center">
+
+    <!--Main content:-->
+    <main class="container">
+
+      <!-- SIDEBAR SECTION -->
+      <AppSidebar />
+
+      <!-- CHAT SECTION -->
+
+    </main>
 
   </div>
 </template>
---
+++ src/components/AppSidebar.vue
@@ -0,0 +1,21 @@
+<script >
+	//LOGIC HERE...
+</script>
+
+<template>
+    <section id="sidebar" class="col-4 p-0 h-100 bg-light-gray">
+        <h1>Hello world</h1>
+    </section>
+</template>
+
+<style scoped>
+/* --- STYLE HERE... --- */
+
+/*CSS reset*/
+
+/*Pseudo-classes*/
+
+/*Generics*/
+
+/*Utilties*/
+</style>
