378b86c406eb3cbb72838cfe1b0e61044588c157
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-08-19 19:05:39 +0200

    media fix

--- src/components/AppSidebar.vue
+++ src/components/AppSidebar.vue
@@ -6,7 +6,19 @@
     <section id="sidebar" class="col-4 p-0 h-100 bg-light-gray">
 
         <!-- User section -->
-        <div id="user-section" class="flex-shrink-0">User</div>
+        <div id="user-section" class="flex-shrink-0 d-flex justify-content-between">
+            <div class="media">
+                <figure>
+                    <img src="../assets/img/avatar_io.jpg" alt="Silvia Rivella">
+                </figure>
+                <div class="h6 media-text">Silvia Rivella</div>
+            </div>
+            <div class="w-25 text-muted d-flex justify-content-around align-items-center">
+                <i class="fa-solid fa-circle-notch" role="button"></i>
+                <i class="fa-solid fa-comment-alt" role="button"></i>
+                <i class="fa-solid fa-ellipsis-v" role="button"></i>
+            </div>
+        </div>
 
         <!-- Notifications -->
         <div id="notifications" class="bg-light-blue flex-shrink-0">Notifications</div>
