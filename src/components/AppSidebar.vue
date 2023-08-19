511a5a14ce63ec18c626ca494f72cfd378477265
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-08-19 18:54:46 +0200

    content appsidebar

--- src/components/AppSidebar.vue
+++ src/components/AppSidebar.vue
@@ -4,6 +4,19 @@
 
 <template>
     <section id="sidebar" class="col-4 p-0 h-100 bg-light-gray">
+
+        <!-- User section -->
+        <div id="user-section" class="flex-shrink-0">User</div>
+
+        <!-- Notifications -->
+        <div id="notifications" class="bg-light-blue flex-shrink-0">Notifications</div>
+
+        <!-- Search contacts -->
+        <div id="search-contacts" class="p-2 flex-shrink-0">Search contacts</div>
+
+        <!-- Contacts list -->
+        <div id="contacts-list" class="flex-grow-1 bg-white overflow-auto">Contacts list</div>
+
     </section>
 </template>
 
