fb84d509805e605c9c28062d7b914dc3c0a14ef2
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-08-19 18:44:25 +0200

    content add to appchat

--- src/components/AppChatSection.vue
+++ src/components/AppChatSection.vue
@@ -3,7 +3,17 @@
 </script>
 
 <template>
-    <section id="chat" class=" col-8 p-0 h-100 bg-chat-color">
+    <section id="chat" class="col-8 p-0 h-100 bg-chat-color d-flex flex-column">
+
+        <!-- Chat header -->
+        <header class="bg-light-gray flex-shrink-0">header</header>
+
+        <!-- Chat messages -->
+        <section class="flex-grow-1 overflow-auto bg-chat-image">messages</section>
+
+        <!-- Chat footer -->
+        <footer class="flex-shrink-0">Footer</footer>
+
     </section>
 </template>
 
