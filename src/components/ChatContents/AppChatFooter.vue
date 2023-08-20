6ef425768153da85512b2d5e35a585915e6dfaf8
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-08-20 15:39:29 +0200

    chatFooter

--- src/components/ChatContents/AppChatFooter.vue
+++ src/components/ChatContents/AppChatFooter.vue
@@ -4,17 +4,18 @@
 
 <template>
     <!-- Chat footer -->
-    <footer class="flex-shrink-0">Footer</footer>
+    <footer class="flex-shrink-0 container-fluid">
+        <div class="row h-100 align-items-center">
+            <div class="col-1 d-flex justify-content-center">
+                <i class="fa-regular fa-smile fa-2x text-muted" role="button"></i>
+            </div>
+            <div class="col-10">
+                <input type="text" class="form-control" placeholder="Scrivi un messaggio">
+            </div>
+            <div class="col-1 d-flex justify-content-center">
+                <i class="fa-solid fa-microphone fa-2x text-muted" role="button"></i>
+            </div>
+        </div>
+    </footer>
 </template>
 
-<style>
-/* --- STYLE HERE... --- */
-
-/*CSS reset*/
-
-/*Pseudo-classes*/
-
-/*Generics*/
-
-/*Utilties*/
-</style>
