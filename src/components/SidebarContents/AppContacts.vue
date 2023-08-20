6307483953fb819c31f3d422de98fe24f4eac73b
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-08-20 15:14:05 +0200

    contacts fixed

--- src/components/SidebarContents/AppContacts.vue
+++ src/components/SidebarContents/AppContacts.vue
@@ -4,7 +4,18 @@
 
 <template>
     <!-- Contacts list -->
-    <div id="contacts-list" class="flex-grow-1 bg-white overflow-auto">Contacts list</div>
+    <div id="contacts-list" class="flex-grow-1 bg-white overflow-auto">
+        <ul class="m-0 p-0">
+            <li class="media">
+                <figure>
+                    <img src="../../assets/img/avatar_2.jpg" alt="Gabriele Lombardo">
+                </figure>
+                <div class="h6 media-text">
+                    <div>Gabriele Lombardo</div>
+                </div>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <style>
